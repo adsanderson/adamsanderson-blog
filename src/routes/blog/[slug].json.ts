90d0716f720101ab57457b6ed0f9aabe5c8bc8a6
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-04 19:36:36 +0000

    update ??????

--- src/routes/blog/[slug].json.ts
+++ src/routes/blog/[slug].json.ts
@@ -2,13 +2,13 @@ import { process } from '$lib/markdown';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async (request) => {
-  console.log('SLUG ------------------------------');
   const { slug } = request.params;
 
-
-
   const { metadata, content } = process(`src/posts/${slug}.md`);
+  console.log('METADATA:', metadata)
   const body = JSON.stringify({ metadata, content });
+
+  console.log('BOFY', body);
 
   return {
     body

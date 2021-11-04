74b0e4fb6e9392a03308f53ba922dc42f804c2e1
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-04 19:36:36 +0000

    update ??????

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,14 +1,16 @@
 <script context="module" lang="ts">
 	import { base } from '$app/paths';
 	import type { Markdown } from '$lib/types';
-
+	export const prerender = true;
 	console.log('test');
 
 	export async function load({ page, fetch }) {
-		console.log('test 222222');
 		const slug = page.params.slug;
-		console.log('test 222222', page, slug);
-		const post = await fetch(`${base}/blog/${slug}.json`).then((r) => r.json());
+
+		const post = await fetch(`${base}/blog/${slug}.json`).then((r) =>
+			r.json().catch((err) => console.log('HERE????????????????', err))
+		);
+		console.log('POST', post);
 		return {
 			props: { post }
 		};
@@ -20,16 +22,11 @@
 	let date = post.metadata.date.toUpperCase();
 </script>
 
+<!-- 
 <svelte:head>
 	<title>{post.metadata.title}</title>
 </svelte:head>
 
 <h1 class="title">{post.metadata.title}</h1>
-<p class="info"><a href="https://github.com/zhuzilin">zhuzilin</a> {date}</p>
-{@html post.content}
 
-<style lang="css">
-	h1.title {
-		margin-bottom: 0;
-	}
-</style>
+{@html post.content} -->

5d2029bb0444a8f6dfc544091892fbb554a0713b
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-07 09:19:29 +0000

    Add favico and tidy

--- src/routes/blog/[slug].json.ts
+++ src/routes/blog/[slug].json.ts
@@ -5,10 +5,7 @@ export const get: RequestHandler = async (request) => {
   const { slug } = request.params;
 
   const { metadata, content } = await process(slug);
-  console.log('METADATA:', metadata)
   const body = JSON.stringify({ metadata, content });
-
-  console.log('BOFY', body);
 
   return {
     body

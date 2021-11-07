d7122d9e74f7c8dcbc776bc008c88414009724cd
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-07 21:33:06 +0000

    fix checks

---
+++ src/routes/tag/[tag].svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	import { base } from '$app/paths';
+	// import { page } from '$app/stores';
+	// import type { Markdown } from '$lib/types';
+	export const prerender = true;
+
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch }) {
+		const tag = page.params.tag;
+		await fetch(`${base}/tag/${tag}.json`).then((r) => r.json());
+		// return {
+		// 	props: { posts }
+		// };
+		return {
+			props: {
+				tag: tag
+			}
+		};
+	}
+</script>
+
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let tag;
+</script>
+
+<h1>{tag}</h1>

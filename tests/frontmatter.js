694aab3638bac15093b05d5cb1647c03e6f87d9b
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-09 19:21:02 +0000

    Add tasks

--- tests/frontmatter.js
+++ tests/frontmatter.js
@@ -4,7 +4,6 @@ import * as assert from 'uvu/assert';
 test('Math.sqrt()', () => {
     assert.is(Math.sqrt(4), 2);
     assert.is(Math.sqrt(144), 12);
-    assert.is(Math.sqrt(2), Math.SQRT2);
 });
 
 test.run();

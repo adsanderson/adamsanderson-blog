b925091038b4b8ff42826b0cdc5b33450a51a3ca
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-11 20:53:08 +0000

    delete test for now

--- tests/frontmatter.js
+++
@@ -1,9 +0,0 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
-
-test('Math.sqrt()', () => {
-    assert.is(Math.sqrt(4), 2);
-    assert.is(Math.sqrt(144), 12);
-});
-
-test.run();

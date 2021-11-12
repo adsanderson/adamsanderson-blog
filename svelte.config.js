d8dd939d8142123a449ca6b98be9a3ee24f72346
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-12 11:46:35 +0000

    switch to static site for now

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,8 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 
-import netlifyAdapter from '@sveltejs/adapter-netlify';
-// import adapter from '@sveltejs/adapter-static';
+// import netlifyAdapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,16 +14,16 @@ const config = {
 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		adapter: netlifyAdapter(),
-		// adapter: adapter({
-		// 	// default options are shown
-		// 	pages: 'build',
-		// 	assets: 'build',
-		// 	fallback: null
-		// }),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		// adapter: netlifyAdapter(),
+		adapter: adapter({
+			// 	// default options are shown
+			// 	pages: 'build',
+			// 	assets: 'build',
+			// 	fallback: null
+			// }),
+			// hydrate the <div id="svelte"> element in src/app.html
+			target: '#svelte'
+		}
 };
 
-export default config;
+	export default config;

3eaaa65964f483e1178a69dd62a05dac82f9677e
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-12 11:49:37 +0000

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -16,14 +16,14 @@ const config = {
 	kit: {
 		// adapter: netlifyAdapter(),
 		adapter: adapter({
-			// 	// default options are shown
-			// 	pages: 'build',
-			// 	assets: 'build',
-			// 	fallback: null
-			// }),
-			// hydrate the <div id="svelte"> element in src/app.html
-			target: '#svelte'
-		}
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
-	export default config;
+export default config;

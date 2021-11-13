a4df93b0812237730ee5c91bd760bb28793ceb08
Adam Sanderson <adam@adamsanderson.co.uk>
2021-11-13 15:19:51 +0000

    try netlify adapter again

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,8 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 
-// import netlifyAdapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-static';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,13 +14,18 @@ const config = {
 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		// adapter: netlifyAdapter(),
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: netlifyAdapter(),
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null
+		// }),
+		prerender: {
+			crawl: true,
+			enabled: true,
+			// pages: ['*'],
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}

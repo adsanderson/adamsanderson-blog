0f12258588cdafbb7a366a25a51234db3102b584
Adam Sanderson <adam@adamsanderson.co.uk>
2023-05-26 15:51:45 +0000

    get ready

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
+import { plugin as markdown, Mode } from 'vite-plugin-markdown'
 
 export default {
-  plugins: [ sveltekit() ]
+  plugins: [sveltekit(), markdown({ mode: [Mode.HTML, Mode.TOC], })]
 };

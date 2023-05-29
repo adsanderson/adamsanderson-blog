37e5f214d5fe31cfd4a5af99e16c66cb424a0762
Adam Sanderson <adam@adamsanderson.co.uk>
2023-05-29 11:13:41 +0000

    prerender feed

--- src/lib/mdToHtml.js
+++ src/lib/mdToHtml.js
@@ -1,24 +1,17 @@
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
-import frontmatter from 'remark-frontmatter'
-import remarkGfm from 'remark-gfm'
+
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
-import { matter } from 'vfile-matter'
 
 /**
  * 
  */
 export async function mdToHtml(content) {
-
-
     return unified()
         .use(remarkParse)
-        .use(frontmatter, ['yaml'], (data) => {
-            console.log(data)
-        })
-        .use(() => (_, file) => { matter(file) })
         .use(remarkRehype)
+        .use(rehypeStringify)
         .use(rehypeStringify)
         .process(content)
 }

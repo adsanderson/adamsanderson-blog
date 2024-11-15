3045313de8e2bd56c05ce2cf42ed8317cfed3cdb
Adam Sanderson <adam@adamsanderson.co.uk>
2024-11-15 17:10:39 +0000

    Update DSL name

--- e2e/adamsanderson.drivers.ts
+++ e2e/adamsanderson.drivers.ts
@@ -1,24 +1,25 @@
 import { type Page } from 'playwright';
-import { type AdamSandersonCoUk } from './adamsanderson.dsl';
+import { type AdamSandersonBlog } from './adamsanderson.dsl';
 import { expect } from '@playwright/test';
 
-export class AdamSandersonCoUkWeb implements AdamSandersonCoUk {
-    private page: Page;
+export class AdamSandersonCoUkWeb implements AdamSandersonBlog {
+	private page: Page;
 
-    constructor(page: Page) {
-        this.page = page;
-    }
-    async goto() {
-        await this.page.goto('https://adamsanderson.co.uk');
-    }
-    async expectToSeePosts() {
-        await expect(this.page.getByRole('link', { name: 'From Bootstrap - How to make a point with CSS' })).toBeVisible();
-    }
-    async expectToSeePost(postTitle: string) { }
+	constructor(page: Page) {
+		this.page = page;
+	}
+	async goto() {
+		await this.page.goto('https://adamsanderson.co.uk');
+	}
+	async expectToSeePosts() {
+		await expect(
+			this.page.getByRole('link', { name: 'From Bootstrap - How to make a point with CSS' })
+		).toBeVisible();
+	}
+	async expectToSeePost(postTitle: string) {}
 
-
-
-    async viewPost(postTitle: string) {
-        await this.page.goto(`/posts/${postTitle}`);
-    }
+	async viewPost(postTitle: string) {
+		await this.page.goto(`/posts/${postTitle}`);
+	}
 }
+

f9537116240927fa3954c366811cb625aae0e102
Adam Sanderson <adam@adamsanderson.co.uk>
2024-12-19 15:35:18 +0000

    Test tracing

--- src/lib/bloglist.js
+++ src/lib/bloglist.js
@@ -1,19 +1,27 @@
+import { trace } from '@opentelemetry/api';
+
+const tracer = trace.getTracer('adamsanderson-blog')
+
 /**
  * @param {"published" | "unpublished" | "all" | string} filterState
  * @param {{[key: string]: any}[]} blogs
  * @returns
  */
 export function processBlogList(filterState, blogs) {
-	blogs = blogs.filter(toFilterState(filterState));
-	return blogs.sort((a, b) => {
-		if (a.publishDate < b.publishDate) {
-			return 1;
-		}
-		if (a.publishDate > b.publishDate) {
-			return -1;
-		}
-		return 0;
-	});
+	return tracer.startActiveSpan('Build blogpost list', (span) => {
+		blogs = blogs.filter(toFilterState(filterState));
+		const sorted = blogs.sort((a, b) => {
+			if (a.publishDate < b.publishDate) {
+				return 1;
+			}
+			if (a.publishDate > b.publishDate) {
+				return -1;
+			}
+			return 0;
+		});
+		span.end();
+		return sorted;
+	})
 }
 
 function toFilterState(filterState) {

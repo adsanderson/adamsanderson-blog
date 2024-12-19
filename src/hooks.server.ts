9e19d377cb4c38ad28df0dfe212db9588a26936c
Adam Sanderson <adam@adamsanderson.co.uk>
2024-12-19 11:15:42 +0000

    add sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,18 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { logger } from '$lib/logger';
 
-/** @type {import('@sveltejs/kit').HandleServerError} */
-export async function handleError({ error, event, status, message }) {
+Sentry.init({
+	dsn: 'https://f58d6e56c275f1974b3200170ecb311f@o4507613218799616.ingest.de.sentry.io/4508494498103376',
+	tracesSampleRate: 1
+});
+
+export const handleError = Sentry.handleErrorWithSentry(async function _handleError({
+	error,
+	event,
+	status,
+	message
+}) {
 	const errorId = crypto.randomUUID();
 
 	logger.error(error, `Error ID: ${message} ${errorId}`);
@@ -10,4 +21,6 @@ export async function handleError({ error, event, status, message }) {
 		message: 'Whoops!',
 		errorId
 	};
-}
+});
+
+export const handle = sequence(Sentry.sentryHandle());

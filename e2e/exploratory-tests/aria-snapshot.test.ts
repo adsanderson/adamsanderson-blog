0f00cfbfa0b615b6132422459d0bfd8bd0a22d03
Adam Sanderson <adam@adamsanderson.co.uk>
2025-02-13 11:09:02 +0000

    Add aria snapshots for all posts

--- e2e/exploratory-tests/aria-snapshot.test.ts
+++ e2e/exploratory-tests/aria-snapshot.test.ts
@@ -1,4 +1,4 @@
-import { test } from '../fixtures';
+import { expect, test } from '../fixtures';
 
 test(
 	'should display the page in different color schemes',
@@ -6,21 +6,28 @@ test(
 		tag: ['@exploratory']
 	},
 	async ({ page, adamSandersonCoUk, logger }) => {
+		test.slow();
+
 		await adamSandersonCoUk.listPosts();
 		await adamSandersonCoUk.expectPostsToExist();
 
-		const homeBody = await page.locator('body').ariaSnapshot();
-		logger.info(homeBody);
+		const homeBody = await page.locator('body')
+
+		expect(homeBody).toMatchAriaSnapshot({ name: 'home' });
 
 		const postLinks = await page
 			.getByRole('article', { name: 'Blog Posts' })
 			.getByRole('link')
-			.all();
+			.all()
 
-		for (const link of postLinks) {
-			await link.click();
-			const postBody = await page.locator('body').ariaSnapshot();
-			logger.info(postBody);
+		const postHrefs = await Promise.all(postLinks.map((x) => x.getAttribute('href')))
+
+		for (const href of postHrefs) {
+			if (!href) throw new Error('No href found on post link');
+			
+			await page.goto(href)
+			const postBody = await page.locator('body')
+			await expect(postBody).toMatchAriaSnapshot({ name: href });
 		}
 	}
 );
